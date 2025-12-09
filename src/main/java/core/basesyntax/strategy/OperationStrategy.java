c0514ec8cb3b5dd6a7469b73d6432bb04aa942d0
Szymon Twardowski <szytwardowski@gmail.com>
2025-12-09 19:09:21 +0100

    shop#5.0

--- src/main/java/core/basesyntax/strategy/OperationStrategy.java
+++ src/main/java/core/basesyntax/strategy/OperationStrategy.java
@@ -1,8 +1,7 @@
 package core.basesyntax.strategy;
 
-import core.basesyntax.model.FruitTransaction; // Zmieniona kolejność
+import core.basesyntax.model.FruitTransaction;
 import core.basesyntax.service.operation.OperationHandler;
-import java.util.Map;
 
 public interface OperationStrategy {
     OperationHandler getHandler(FruitTransaction.Operation operation);
